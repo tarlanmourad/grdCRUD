958659c859988a2c8b7257c2ac6c5bcf06b30f01
Tarlan Muradli <terlanmuradli075@mail.ru>
2022-12-11 12:52:46 +0400

    Add project files.

---
+++ index.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="grdCRUD.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="CSS/style.css" rel="stylesheet" />
+    <title>CRUD</title>
+</head>
+<body>
+    <form id="frmCRUD" runat="server">
+        <div>
+            <asp:GridView ID="grdView" runat="server" OnRowCancelingEdit="grdView_RowCancelingEdit" 
+                OnRowDeleting="grdView_RowDeleting" OnRowEditing="grdView_RowEditing" 
+                OnRowUpdating="grdView_RowUpdating" AutoGenerateColumns="False" CssClass="mydatagrid" 
+                PagerStyle-CssClass="pager" HeaderStyle-CssClass="header" RowStyle-CssClass="rows">
+                <Columns>
+                    <asp:TemplateField HeaderText="ID">
+                        <ItemTemplate>
+                            <asp:Label ID="lblID" runat="server" Text='<%#Eval("ProductID")%>'></asp:Label>
+                        </ItemTemplate>
+                    </asp:TemplateField>
+
+                    <asp:BoundField DataField="ProductName" HeaderText="Product Name" />
+                    <asp:BoundField DataField="QuantityPerUnit" HeaderText="Quantity" />
+                    <asp:BoundField DataField="UnitPrice" HeaderText="Price" />
+                    <asp:BoundField DataField="UnitsInStock" HeaderText="Stock" />
+                    <asp:CommandField ShowEditButton="true" />
+                    <asp:CommandField ShowDeleteButton="true" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ index.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace grdCRUD
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection("Data Source=TARLAN\\LOCALHOST;Initial Catalog=Northwind;Integrated Security=True");
+        SqlCommand cmd;
+        String query;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) BindGrid();
+        }
+
+        protected void grdView_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            grdView.EditIndex = -1;
+            BindGrid();
+        }
+
+        protected void grdView_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            Label id = grdView.Rows[e.RowIndex].FindControl("lblID") as Label;
+            conn.Open();
+            query = "DELETE FROM Products WHERE ProductID=@ID";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@ID", Convert.ToInt32(id.Text));
+
+            if(cmd.ExecuteNonQuery() == 1)
+            {
+                string script = "alert(\"Deleted row\");";
+                ScriptManager.RegisterStartupScript(this, GetType(), "ServerControlScript", script, true);
+            }
+            else
+            {
+                string script = "alert(\"Not deleted\");";
+                ScriptManager.RegisterStartupScript(this, GetType(), "ServerControlScript", script, true);
+            }
+
+            conn.Close();
+            grdView.EditIndex = -1;
+            BindGrid();
+        }
+
+        protected void grdView_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            grdView.EditIndex = e.NewEditIndex;
+            BindGrid();
+        }
+
+        protected void grdView_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            Label id = grdView.Rows[e.RowIndex].FindControl("lblID") as Label;
+
+            string name = ((TextBox)grdView.Rows[e.RowIndex].Cells[1].Controls[0]).Text.ToString();
+            string quantity = ((TextBox)grdView.Rows[e.RowIndex].Cells[2].Controls[0]).Text.ToString();
+            string price = ((TextBox)grdView.Rows[e.RowIndex].Cells[3].Controls[0]).Text.ToString();
+            string stock = ((TextBox)grdView.Rows[e.RowIndex].Cells[4].Controls[0]).Text.ToString();
+
+            conn.Open();
+            query = "UPDATE Products SET ProductName=@name, QuantityPerUnit=@quantity, UnitPrice=@price, UnitsInStock=@stock WHERE ProductID=@ID";
+            cmd = new SqlCommand(query, conn);
+            cmd.Parameters.AddWithValue("@ID", Convert.ToInt32(id.Text));
+            cmd.Parameters.AddWithValue("@name", name);
+            cmd.Parameters.AddWithValue("@quantity", quantity);
+            cmd.Parameters.AddWithValue("@price", price);
+            cmd.Parameters.AddWithValue("@stock", stock);
+
+            if (cmd.ExecuteNonQuery() == 1)
+            {
+                string script = "alert(\"Updated row\");";
+                ScriptManager.RegisterStartupScript(this, GetType(), "ServerControlScript", script, true);
+            }
+            else
+            {
+                string script = "alert(\"Not updated\");";
+                ScriptManager.RegisterStartupScript(this, GetType(), "ServerControlScript", script, true);
+            }
+
+            conn.Close();
+            grdView.EditIndex = -1;
+            BindGrid();
+        }
+
+        private void BindGrid()
+        {
+            conn.Open();
+            query = "SELECT ProductID, ProductName, QuantityPerUnit, UnitPrice, UnitsInStock FROM Products";
+            cmd = new SqlCommand(query, conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            grdView.DataSource = ds;
+            grdView.DataBind();
+            conn.Close();
+        }
+
+
+    }
+}
